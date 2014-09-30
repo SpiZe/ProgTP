b5d8a4a5b9da34fb6900fdb78d18340ebc1086b8
Samuel <samgacha@hotmail.com>
2014-09-30 14:46:37 -0700

    Création des attributs de Token

--- ProgTP/src/Models/Token.java
+++ ProgTP/src/Models/Token.java
@@ -1,5 +1,47 @@
 package Models;
 
 public class Token {
+	private enum Status
+	{
+		Player1, Player2, Empty
+	}
+	private int posY; 
+	private String imgPath; 
+	private int posX;
+	private Status tokenStatus; 
+	
+	public int getPosX() {
+		return posX;
+	}
+	
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+	
+	public int getPosY() {
+		return posY;
+	}
+	
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+	
+	public String getImgPath() {
+		return imgPath;
+	}
+	
+	public void setImgPath(String imgPath) {
+		this.imgPath = imgPath;
+	}
 
+	public Status getTokenStatus() {
+		return tokenStatus;
+	}
+
+	public void setTokenStatus(Status tokenStatus) {
+		this.tokenStatus = tokenStatus;
+	}
+	
+	
+	
 }
