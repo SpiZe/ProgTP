b5dbc1062cebd4a556ecf896dcf190a308264dfb
Samuel <samgacha@hotmail.com>
2014-09-30 15:37:22 -0700

    Mise a jours Token

--- ProgTP/src/Models/Token.java
+++ ProgTP/src/Models/Token.java
@@ -5,26 +5,8 @@ public class Token {
 	{
 		Player1, Player2, Empty
 	}
-	private int posY; 
-	private int posX;
 	private String imgPath; 
 	private Status tokenStatus; 
-	
-	public int getPosX() {
-		return posX;
-	}
-	
-	public void setPosX(int posX) {
-		this.posX = posX;
-	}
-	
-	public int getPosY() {
-		return posY;
-	}
-	
-	public void setPosY(int posY) {
-		this.posY = posY;
-	}
 	
 	public String getImgPath() {
 		return imgPath;
